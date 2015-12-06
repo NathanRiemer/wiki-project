5baea96142d54bc8ee8dc1b7e37be36b8888f5e2
Nathan Riemer <nathan.riemer@gmail.com>
2015-12-06 12:12:43 -0500

    Improves links on revision index page.

--- models/revision.rb
+++ models/revision.rb
@@ -4,8 +4,11 @@ class Revision < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   def mod_history (word)
-    "#{word} by #{user.username} on #{created_at.strftime("%A, %B %d, %Y at %l:%M %p")}"
+    "#{word} by <a href='/users/#{user.id}'>#{user.username}</a> on #{created_at.strftime("%A, %B %d, %Y at %l:%M %p")}"
   end
 
+  def mod_history_with_date_link (word)
+    "#{word} by <a href='/users/#{user.id}'>#{user.username}</a> on <a href='/articles/#{article.id}/revisions/#{id}'>#{created_at.strftime("%A, %B %d, %Y at %l:%M %p")}</a>"
+  end
 
 end
